3c66bc5e23a9f73f66846192feae9b6552328098
tolbarweb <tolbarweb@gmail.com>
2021-04-08 10:57:59 -0500

    ffzz

--- lib/score.py
+++ lib/score.py
@@ -1,38 +1,106 @@
-import joblib
+import time
+from absl import app, logging
+import cv2
+import numpy as np
+import tensorflow as tf
+from yolov3_tf2.models import (
+    YoloV3, YoloV3Tiny
+)
+from yolov3_tf2.dataset import transform_images, load_tfrecord_dataset
+from yolov3_tf2.utils import draw_outputs
+from flask import  Response, jsonify, abort
+import os
 from azureml.core.model import Model
-
 from azureml.contrib.services.aml_request import AMLRequest, rawhttp
 from azureml.contrib.services.aml_response import AMLResponse
-from PIL import Image
-import json
-from yolov3_tf2.models import YoloV3
 
 def init():
-    global model
-    model_path = Model.get_model_path("yolov3-tf")
-    print("Model Path is  ", model_path)
-    #model = joblib.load(model_path)
+    os.system('ls -la')
+    os.system('pwd')
 
-    num_classes = 80
-    yolo = YoloV3(classes=num_classes)
-    yolo.load_weights(model_path).expect_partial()
+    global yolo, class_names, classes_path, weights_path, tiny, size, output_path, num_classes, physical_devices
+
+    # customize your API through the following parameters
+    classes_path = './data/labels/coco.names'
+    weights_path = Model.get_model_path("yolov3-tf")
+    print("Model Path is  ", weights_path)
+    tiny = False                    # set to True if using a Yolov3 Tiny model
+    size = 416                      # size images are resized to for model
+    output_path = './detections/'   # path to output folder where images with detections are saved
+    num_classes = 80                # number of classes in model
+
+    os.system('ls -la')
+    os.system('pwd')
+
+    # load in weights and classes
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    if len(physical_devices) > 0:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+    if tiny:
+        yolo = YoloV3Tiny(classes=num_classes)
+    else:
+        yolo = YoloV3(classes=num_classes)
+
+    yolo.load_weights(weights_path).expect_partial()
     print('weights loaded')
-    
+
+    class_names = [c.strip() for c in open(classes_path).readlines()]
+    print('classes loaded')
+
 @rawhttp
 def run(request):
-    print("This is run()")
+    raw_images = []
+    images = request.files.getlist("images")
+    image_names = []
+    for image in images:
+        image_name = image.filename
+        image_names.append(image_name)
+        image.save(os.path.join(os.getcwd(), image_name))
+        img_raw = tf.image.decode_image(
+            open(image_name, 'rb').read(), channels=3)
+        raw_images.append(img_raw)
+        
+    num = 0
     
-    if request.method == 'GET':
-        # For this example, just return the URL for GETs.
-        respBody = str.encode(request.full_path)
-        return AMLResponse(respBody, 200)
-    elif request.method == 'POST':
-        file_bytes = request.files["image"]
-        image = Image.open(file_bytes).convert('RGB')
-        # For a real-world solution, you would load the data from reqBody
-        # and send it to the model. Then return the response.
+    # create list for final response
+    response = []
 
-        # For demonstration purposes, this example just returns the size of the image as the response..
-        return AMLResponse(json.dumps(image.size), 200)
-    else:
-        return AMLResponse("bad request", 500)
+    for j in range(len(raw_images)):
+        # create list of responses for current image
+        responses = []
+        raw_img = raw_images[j]
+        num+=1
+        img = tf.expand_dims(raw_img, 0)
+        img = transform_images(img, size)
+
+        t1 = time.time()
+        boxes, scores, classes, nums = yolo(img)
+        t2 = time.time()
+        print('time: {}'.format(t2 - t1))
+
+        print('detections:')
+        for i in range(nums[0]):
+            print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
+                                            np.array(scores[0][i]),
+                                            np.array(boxes[0][i])))
+            responses.append({
+                "class": class_names[int(classes[0][i])],
+                "confidence": float("{0:.2f}".format(np.array(scores[0][i])*100))
+            })
+        response.append({
+            "image": image_names[j],
+            "detections": responses
+        })
+        img = cv2.cvtColor(raw_img.numpy(), cv2.COLOR_RGB2BGR)
+        img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
+        cv2.imwrite(output_path + 'detection' + str(num) + '.jpg', img)
+        print('output saved to: {}'.format(output_path + 'detection' + str(num) + '.jpg'))
+
+    #remove temporary images
+    for name in image_names:
+        os.remove(name)
+    try:
+        return jsonify({"response":response}), 200
+    except FileNotFoundError:
+        abort(404)
